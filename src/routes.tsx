87d496fd9272b76f1ae7aa6227b6586a7628154f
DenisGridin <denisgridin6@gmail.com>
2020-04-26 22:51:53 +0300

    add bulding three js

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { AuthPage } from './pages'
+import { BuildingPage } from './pages'
 
 type RoutesProps = boolean
 
+// export const useRoutes: React.FC<RoutesProps> = isAuth => {
+//   if (isAuth) {
+//     return (
+//       <Switch>
+//         <Route path='/home' component={() => <h1>HOME</h1>} />
+//         <Redirect to='/home' />
+//       </Switch>
+//     )
+//   }
+//   return (
+//     <Switch>
+//       <Route path='/login' component={AuthPage} exact />
+//       <Redirect to='/login' />
+//     </Switch>
+//   )
+// }
 export const useRoutes: React.FC<RoutesProps> = isAuth => {
-  if (isAuth) {
-    return (
-      <Switch>
-        <Route path='/home' component={() => <h1>HOME</h1>} />
-        <Redirect to='/home' />
-      </Switch>
-    )
-  }
   return (
     <Switch>
-      <Route path='/login' component={AuthPage} exact />
-      <Redirect to='/login' />
+      <Route path='/house' component={BuildingPage} />
+      <Redirect to='/house' />
     </Switch>
   )
 }
